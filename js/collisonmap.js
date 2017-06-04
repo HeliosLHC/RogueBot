491f6a4522aa311149eb9f4daa16dbf8dfd1668a
garyho1 <garyho321@gmail.com>
2017-06-04 14:38:08 -0400

    added collison map data

--- js/collisonmap.js
+++ js/collisonmap.js
@@ -1,5 +1,52 @@
 // TODO Convert to JSON Format
 var collisionMapArray = [
+    platform1 = [
+        //Section 1 
+        {
+            topBound: 416,
+            bottomBound: 512,
+            leftBound: 64,
+            rightBound: 96,
+            solid: true,
+			floating: false
+        }
+        //Section 2
+        {
+            topBound: 448,
+            bottomBound: 512,
+            leftBound: 96,
+            rightBound: 160,
+            solid: true,
+			floating: false
+        }
+        //Section 3
+        {
+            topBound: 416,
+            bottomBound: 512,
+            leftBound: 160,
+            rightBound: 192,
+            solid: true,
+			floating: false
+        }
+        //Section 4
+        {
+            topBound: 448,
+            bottomBound: 512,
+            leftBound: 192,
+            rightBound: 384,
+            solid: true,
+			floating: false
+        }
+    ]
+    ,
+    fplatform1 = {
+        topBound: 384,
+        bottomBound: 416,
+        leftBound: 256,
+        rightBound: 224,
+        solid: true,
+        floating: true
+    }
 	stairs1 = [
 		// Section 1
 		{
